2f7ddb4fa0aa6e8682f404e04e5c7bf9731472ba
Arturo Baldo <baldoarturo@gmail.com>
2019-04-29 21:39:43 -0300

    Added Django

--- reflector/start.sh
+++ reflector/start.sh
@@ -1,2 +1,7 @@
 mkdir -p /stream/${NAME}
-/usr/bin/ffmpeg -re -i ${INPUT} -vcodec copy -acodec copy -hls_flags delete_segments /stream/${NAME}/live.m3u8
+/usr/bin/ffmpeg -re -i ${INPUT} \
+    -bufsize 5000k \
+    -vcodec copy -b:v 1000k \
+    -acodec copy \
+    -hls_flags delete_segments \
+    "'/stream/'"${NAME}"'/live.m3u8'"
