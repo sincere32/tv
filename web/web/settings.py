fe5d068fc8c8e8cd82eceb199596c32166ce14d7
Arturo Baldo <baldoarturo@gmail.com>
2019-05-03 19:15:55 -0300

    Moved media folder

--- web/web/settings.py
+++ web/web/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/tmp/stream/static/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
