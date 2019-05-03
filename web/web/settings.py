009246f0b95fb0d97e45e8e91f474277687359ff
Arturo Baldo <baldoarturo@gmail.com>
2019-05-03 17:05:12 -0300

    Updated settings for static files

--- web/web/settings.py
+++ web/web/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/tmp/stream/static/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 MEDIA_URL = '/media/'
 
