44f38f829e59d350772fb604ee40b8c3d7434b12
Arturo Baldo <baldoarturo@gmail.com>
2019-05-04 12:43:34 -0300

    CSS, fixed player template

--- web/tv/static/player/js/ui.js
+++ web/tv/static/player/js/ui.js
@@ -1,11 +1,35 @@
 $(document).ready(function () {
 
+    function menu_width() {
+        var menu_width = $("#channel-list").width();
+        return menu_width;
+    }
+    function hide_menu() {
+        var menu_width = $("#channel-list").width();
+        $("#channel-list").css("left", -menu_width)
+    }
+    function show_menu() {
+        $("#channel-list").css("left", 0)
+    }
+    hide_menu();
+
     $(".channel-entry").on("click", function () {
         var video = videojs('player');
-        var url = $(this).data("video-url").replace(" ","-");
+        var url = $(this).data("video-url").replace(" ", "-");
         console.log(url);
         video.src(url);
         video.play();
     });
 
+    $(document).mousemove(function (e) {
+        var width = menu_width();
+        var x = e.pageX;
+        var y = e.pageY;
+        if(x<=width) {
+            show_menu();
+        } else {
+            hide_menu();
+        }
+        console.log(x);
+    });
 });
