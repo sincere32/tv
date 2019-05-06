e779ac38e0b1718201b7db055320daed55f392d5
Arturo Baldo <baldoarturo@gmail.com>
2019-05-06 13:59:05 -0300

    Fixed start.sh

--- nginx/build.sh
+++ nginx/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+docker kill tv-nginx
 docker rm tv-nginx
 docker build --rm -t tv/nginx:latest --squash .
 docker image prune -f
-docker run -d --name tv-nginx -p 30000:80 -v stream:/usr/share/nginx/html/stream  tv/nginx
+docker run -d \
+	--name tv-nginx \
+	-p 30000:80 \
+	-v stream:/tmp/stream \
+	-v media:/tmp/media \
+	--hostname=tv-nginx \
+	tv/nginx
--- reflector/start.sh
+++ reflector/start.sh
@@ -1,9 +1,19 @@
 mkdir -p /stream/${NAME}
 rm -rf /stream/${NAME}/*
 
+if ! [ -z $YOUTUBE_DL ]; then
+	echo "Using youtube-dl extractor for " ${YOUTUBE_DL}
+	INPUT=$(youtube-dl -g $YOUTUBE_DL)
+fi
+
+echo "URI is " ${INPUT}
+echo "Starting FFMPEG ..."
+echo ""
+echo ""
+
 /usr/bin/ffmpeg \
 	-re \
-	-i $INPUT \
+	-i ${INPUT} \
 	-bufsize 5000k \
 	-vcodec copy -b:v 700k \
 	-acodec copy \
