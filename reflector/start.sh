433d55e96f88ddf5f8eda481d6c719ff9989719a
Arturo Baldo <baldoarturo@gmail.com>
2019-05-06 12:29:07 -0300

    Changed start.sh for generic input

--- reflector/start.sh
+++ reflector/start.sh
@@ -1,15 +1,6 @@
 mkdir -p /stream/${NAME}
 rm -rf /stream/${NAME}/*
 
-if test -n $YOUTUBE; then
-	echo "Extracting m3u8 URI from YouTube link"
-	echo "Youtube: "$YOUTUBE
-	INPUT=$(youtube-dl -g $(echo $YOUTUBE))
-	echo "m3u8 URI: "$INPUT
-fi
-
-echo "Starting ffmpeg ..."
-
 /usr/bin/ffmpeg \
 	-re \
 	-i ${INPUT} \
