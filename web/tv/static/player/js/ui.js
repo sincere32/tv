304bf92b755f5e1047feeecba31090ddb27d4b73
Arturo Baldo <baldoarturo@gmail.com>
2019-05-07 18:28:01 -0300

    Fixed space on ui.js

--- web/tv/static/player/js/ui.js
+++ web/tv/static/player/js/ui.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
 
     $(".channel-entry").on("click", function () {
         var video = videojs('player');
-        var url = $(this).data("video-url").replace(" ", "-");
+        var url = $(this).data("video-url").replace(/ /g, "-");
         console.log(url);
         video.src(url);
         video.play();
