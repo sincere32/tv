5b9d330a3e7b7c459d58decccddefe17e8b0a158
Arturo Baldo <baldoarturo@gmail.com>
2019-05-16 16:51:38 -0300

    Fixed channel delete

--- web/tv/views.py
+++ web/tv/views.py
@@ -195,8 +195,8 @@ class ChannelsDelete(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         channel = get_object_or_404(Channel, pk=self.kwargs['pk'])
         client = docker_control.Client(channel)
-        if client.delete_channel():
-            channel.delete()
+        client.delete_channel():
+        channel.delete()
         return redirect(to="/control/channels/")
 
 
