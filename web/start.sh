0ffb229ff8259ca64d50c56b055d8b12c83fc035
Arturo Baldo <baldoarturo@gmail.com>
2019-09-20 11:51:23 -0300

    Fixed start.sh for django

--- web/start.sh
+++ web/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python manage.py createsuperuser --username admin --password admin --email foo@foo.foo
-python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
+python manage.py createsuperuser --username admin --password admin --email foo@foo.foo
+python manage.py collectstatic --noinput
 gunicorn web.wsgi --bind=unix:/tmp/stream/gunicorn.sock --workers=1
 
--- web/web/settings.py
+++ web/web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_createsuperuser
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,9 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_createsuperuser'
 ]
-
-INSTALLED_APPS+= ["django_createsuperuser"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
